e773cf589a9bc5c90d1fd8aa7f4cebd4fe72a89b
Muco Rolle Tresor <mucyotreasure@gmail.com>
2018-02-27 19:55:32 +0200

    commenting the code

--- sokwe_game.py
+++ sokwe_game.py
@@ -45,25 +45,39 @@ def sokwe_game():
             # then we accept it else we give he all ibisokozo we have
             questions = int(input("Ushaka ibisokozo bingahe?: "))
 
-            user_score = 0
-            divice_score = 0
-
             if(questions <= len(ibisokozo)):
                 questions = questions
             else:
                 questions = len(ibisokozo)
 
+            # initialize the user_score and the device score
+            # at the start they equal to zero but they'll increment when they guess
+            # the answer
+            user_score = 0
+            device_score = 0
 
+            # get ibisokozo randomely with sample method from the random module
             ibisokozo_list = sample(list(ibisokozo), questions)
+
 
             for igisokozo in ibisokozo_list:
                 user_response = input(igisokozo + ": ")
 
+                # format the user_response and the device response as the same
+                # inorder to have the same values from the device response and user_response
                 user_response = user_response.strip().lower().replace(' ', '').replace('\'', '')
 
                 device_response = ibisokozo[igisokozo].strip().lower().replace(' ', '').replace('\'', '')
 
+                # while the user_response is not equals to the device response
+                # and while the user response is not equals
+                # to "ndaguhaye"(because the user should want to reply ndaguhaye directly
+                # and because we have the condition of this iteration block below
+                # it'll be valuated to True)
+                # looping until the user get the response or replies ndaguhaye if he replies
+                # ndaguhaye get out of the iteration block and continue
                 while(user_response != device_response and user_response != "ndaguhaye"):
+
                     user_response = input("Uhh!! inyishu siyo ushaka kugerageza kandi? canke wishure *ndaguhaye*: ")
 
                     user_response = user_response.strip().lower().replace(' ', '').replace('\'', '')
