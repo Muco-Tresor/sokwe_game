51b0225557883ab58ec1dd08c1e61a75623961c7
Muco Rolle Tresor <mucyotreasure@gmail.com>
2018-03-08 01:18:37 +0200

    ask user if wants to continue playing

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-from sokwe_game import sokwe
+from sokwe_game import sokwe_game
 
-if(__name__ == '__main__'):
-    sokwe()
 
+if(__name__ == "__main__"):
+    sokwe_game()
+
+
+
+continue_response = input("\n\tUshaka kubandanya? ishura ego/oya: ")
+
+# while the respond ego(yes) we re-executed the code
+# hosted in the sokwe_game function
+while(continue_response.lower() == "ego"):
+    sokwe_game()
+    continue_response = input("\n\tUshaka kubandanya? ishura ego/oya: ")
+
+print("\n\tUrakoze gukina Bye Bye!!!")
--- sokwe_game.py
+++ sokwe_game.py
@@ -24,25 +24,22 @@ def sokwe_game():
         ibisokozo  = {}
 
         # get ibisokozos for files/ibisokozo.txt file
+        # when finished to work with it, it closed that file
         with open("files/ibisokozo.txt") as ibisokozos_file:
 
             for ibisokozo_file in ibisokozos_file:
                 # split igisokozo with the equal symbol and store it in tuple
                 (igisokozo, inyishu) = ibisokozo_file.split("=")
 
-                # format the igisokozo and inyishu by
-                # removing rounded spaces and lowercased them
-                # after store in the in an empty dictionnary created
-                igisokozo = igisokozo
-                inyishu = inyishu
-
+                # store igisokozo and inyishu in dictionnary
                 ibisokozo[igisokozo] = inyishu
 
-        # if for the question the user enter the string value throw an exception
+        # for  the question if the user enter a string value throw an exception
+        # because we accept here integers
         try:
             # Demand the user the number of questions he wants
             # if he demands the number which won't surpass the number of ibisoko we have
-            # then we accept it else we give he all ibisokozo we have
+            # then we accept it, else we give him all ibisokozo we have
             questions = int(input("Ushaka ibisokozo bingahe?: "))
 
             if(questions <= len(ibisokozo)):
@@ -51,8 +48,7 @@ def sokwe_game():
                 questions = len(ibisokozo)
 
             # initialize the user_score and the device score
-            # at the start they equal to zero but they'll increment when they guess
-            # the answer
+            # at the start they equal to zero but they'll increment when user #and device guess the answer
             user_score = 0
             device_score = 0
 
@@ -74,8 +70,7 @@ def sokwe_game():
                 # to "ndaguhaye"(because the user should want to reply ndaguhaye directly
                 # and because we have the condition of this iteration block below
                 # it'll be valuated to True)
-                # looping until the user get the response or replies ndaguhaye if he replies
-                # ndaguhaye get out of the iteration block and continue
+                # looping until the user get the response or replies ndaguhaye #if he replies ndaguhaye get out of the iteration block and continue
                 while(user_response != device_response and user_response != "ndaguhaye"):
 
                     user_response = input("Uhh!! inyishu siyo ushaka kugerageza kandi? canke wishure *ndaguhaye*: ")
@@ -103,7 +98,4 @@ def sokwe_game():
 
 
     except FileNotFoundError:
-        print("Sorry ububiko bwibisokozo ntabuhari tugiye kubikosora")
-
-
-sokwe_game()
+        print("Sorry ububiko bw'ibisokozo ntabuhari tugiye kubikosora")
